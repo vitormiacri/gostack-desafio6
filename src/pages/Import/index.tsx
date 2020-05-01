f58e42c6dd732d25da3cc205045d8adef53a6aec
Vitor Morelli <vitao.miacri@gmail.com>
2020-05-01 16:04:00 -0300

    Add Import page

--- src/pages/Import/index.tsx
+++ src/pages/Import/index.tsx
@@ -23,19 +23,26 @@ const Import: React.FC = () => {
   const history = useHistory();
 
   async function handleUpload(): Promise<void> {
-    // const data = new FormData();
+    const data = new FormData();
 
-    // TODO
+    data.append('file', uploadedFiles[0].file);
 
     try {
-      // await api.post('/transactions/import', data);
+      await api.post('/transactions/import', data);
     } catch (err) {
-      // console.log(err.response.error);
+      console.log(err.response.error);
     }
   }
 
   function submitFile(files: File[]): void {
-    // TODO
+    const { name, size } = files[0];
+    const updatedFile: FileProps = {
+      file: files[0],
+      name,
+      readableSize: filesize(size),
+    };
+
+    setUploadedFiles([...uploadedFiles, updatedFile]);
   }
 
   return (
